c2d259c3557cf97a566c0cb8ddfe93f3aa021f79
CygnusMoci <448111558@qq.com>
2017-12-13 17:47:55 +0800

    header根footer

--- src/main/java/com/moci/blogtest/config/MyWebMvcConfig.java
+++ src/main/java/com/moci/blogtest/config/MyWebMvcConfig.java
@@ -15,10 +15,11 @@ public class MyWebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("/themes/login");
-//        registry.addViewController("/themes/index").setViewName("/themes/index");//首页跳转  如果写入的话 可以跳过登陆验证直接访问index
+//        registry.addViewController("/index").setViewName("/themes/index");//首页跳转  如果写入的话 可以跳过登陆验证直接访问index
         registry.addViewController("/login").setViewName("/themes/login");//登录页面
 
         registry.addViewController("/test").setViewName("/test"); //测试
-//        registry.addViewController("/admin/main").setViewName("admin/main");//后台管理的主页
+        registry.addViewController("/header").setViewName("/themes/header");
+        registry.addViewController("/footer").setViewName("/themes/footer");
     }
 }
